c492e5048e35e3da60e4a94db2c320f6381434c3
zolot <zolot3437@gmail.com>
2015-09-10 01:14:07 +0300

    a

--- static/js/common.js
+++ static/js/common.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-     $(function(){
+    $('#stat').click(function() {
+        if(document.getElementById('stat').checked) {
+            localStorage.setItem('stat', "true");
+        } else {
+            localStorage.setItem('stat', "false");
+        }
+    });
+
+    if (localStorage.getItem('stat') == "true") {
+        document.getElementById("stat").setAttribute('checked', 'checked');
+    } else {
+        $('.rus').hide();
         $('.eng').click(function() {
             $(this).next().toggle();
             $(this).addClass("focus")
         });
-    });
+    }
+    
 });
